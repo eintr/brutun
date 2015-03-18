34f21073b7f9200a3ccc1f8afd03bf87eae49144
牛海青 <haiqing1@staff.sina.com.cn>
2015-03-18 17:22:41 +0800

    Experiment code.

---
+++ main.c
@@ -0,0 +1,226 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <string.h>
+#include <sys/ioctl.h>
+#include <errno.h>
+
+#include <linux/if.h>
+#include <linux/if_tun.h>
+
+#include "protocol.h"
+
+static char *remote_ip=NULL;
+static int remote_port=0;
+static char *tun_local_addr=NULL;
+static char *tun_peer_addr=NULL;
+
+static void parse_args(int argc, char **argv)
+{
+	int c;
+
+	do {
+		c=getopt(argc, argv, "R:P:l:p:");
+		switch (c) {
+			case 'R':
+				remote_ip = optarg;
+				break;
+			case 'P':
+				remote_port = strtol(optarg, NULL, 10);
+				break;
+			case 'l':
+				tun_local_addr = optarg;
+				break;
+			case 'p':
+				tun_peer_addr = optarg;
+				break;
+			default:
+				break;
+		}
+	} while (c!=-1);
+
+	if (tun_local_addr==NULL || tun_peer_addr==NULL) {
+		fprintf(stderr, "Usage:!!\n");
+		abort();
+	}
+}
+
+static int tun_alloc(char *dev, int flags) {
+
+	struct ifreq ifr;
+	int fd, err;
+	char *clonedev = "/dev/net/tun";
+
+	/* Arguments taken by the function:
+	 *
+	 * char *dev: the name of an interface (or '\0'). MUST have enough
+	 *   space to hold the interface name if '\0' is passed
+	 * int flags: interface flags (eg, IFF_TUN etc.)
+	 */
+
+	/* open the clone device */
+	if( (fd = open(clonedev, O_RDWR)) < 0 ) {
+		return fd;
+	}
+
+	/* preparation of the struct ifr, of type "struct ifreq" */
+	memset(&ifr, 0, sizeof(ifr));
+
+	ifr.ifr_flags = flags;   /* IFF_TUN or IFF_TAP, plus maybe IFF_NO_PI */
+
+	if (*dev) {
+		/* if a device name was specified, put it in the structure; otherwise,
+		 * the kernel will try to allocate the "next" device of the
+		 * specified type */
+		strncpy(ifr.ifr_name, dev, IFNAMSIZ);
+	}
+
+	/* try to create the device */
+	if ( (err = ioctl(fd, TUNSETIFF, (void *) &ifr)) < 0 ) {
+		close(fd);
+		return err;
+	}
+
+	/* if the operation was successful, write back the name of the
+	 * interface to the variable "dev", so the caller can know
+	 * it. Note that the caller MUST reserve space in *dev (see calling
+	 * code below) */
+	strcpy(dev, ifr.ifr_name);
+
+	/* this is the special file descriptor that the caller will use to talk
+	 * with the virtual interface */
+	return fd;
+}
+
+#define	BUFSIZE	(65536+4096)
+static void relay(int sd, int tunfd, struct sockaddr_in *peer_addr)
+{
+	char buffer[BUFSIZE];
+	int len, ret;
+	pid_t pid;
+	struct sockaddr_in from_addr;
+	socklen_t from_addr_len;
+
+	pid = fork();
+	if (pid==0) {
+		from_addr_len = sizeof(from_addr);
+		while(1) {
+			len = recvfrom(sd, buffer, BUFSIZE, 0, (void*)&from_addr, &from_addr_len);
+			if (len==0) {
+				continue;
+			}
+			if (memcmp(&from_addr, &peer_addr, sizeof(from_addr))!=0) {
+				fprintf(stderr, "Unknown source packet, drop.\n");
+				continue;
+			}
+			while (1) {
+				ret = write(tunfd, buffer, len);
+				if (ret<0) {
+					if (errno==EINTR) {
+						continue;
+					}
+					fprintf(stderr, "write(tunfd): %m\n");
+					exit(1);
+				}
+				if (ret==0) {
+					exit(0);
+				}
+				break;
+			}
+		}
+		exit(0);
+	}
+
+	pid = fork();
+	if (pid==0) {
+		from_addr_len = sizeof(from_addr);
+		while(1) {
+			len = read(tunfd, buffer, BUFSIZE);
+			if (len==0) {
+				continue;
+			}
+			while (1) {
+				ret = sendto(sd, buffer, len, 0, (void*)peer_addr, sizeof(*peer_addr));
+				if (ret<0) {
+					if (errno==EINTR) {
+						continue;
+					}
+					fprintf(stderr, "sendto(sd): %m\n");
+					exit(1);
+				}
+				break;
+			}
+		}
+		exit(0);
+	}
+
+	close(sd);
+	close(tunfd);
+	wait(NULL);
+	wait(NULL);
+}
+
+static int shell(const char *cmd)
+{
+	int ret;
+
+	printf("run: %s  ...  ", cmd);
+	ret = system(cmd);
+	if (ret==-1) {
+		printf("failed.\n");
+	} else {
+		printf("over, status=%d.\n", ret);
+	}
+	return ret;
+}
+
+int
+main(int argc, char **argv)
+{
+	int sd, tun_fd;
+	char tun_name[IFNAMSIZ];
+	struct sockaddr_in local_addr, peer_addr;
+	socklen_t peer_addr_len;
+	char cmdline[BUFSIZE];
+
+	parse_args(argc, argv);
+
+	peer_addr.sin_family = PF_INET;
+	inet_pton(PF_INET, remote_ip, &peer_addr.sin_addr);
+	peer_addr.sin_port = htons(remote_port);
+
+	sd = socket(PF_INET, SOCK_DGRAM, 0);
+	if (sd<0) {
+		perror("socket()");
+		exit(1);
+	}
+
+	local_addr.sin_family = PF_INET;
+	local_addr.sin_addr.s_addr = 0;
+	local_addr.sin_port = htons(SOCKET_PORT);
+	if (bind(sd, (void*)&local_addr, sizeof(local_addr))<0) {
+		perror("bind()");
+		exit(1);
+	}
+
+	tun_name[0]='\0';
+	tun_fd = tun_alloc(tun_name, IFF_TUN|IFF_NO_PI);
+	if (tun_fd<0) {
+		perror("tun_alloc()");
+		exit(1);
+	}
+
+	snprintf(cmdline, BUFSIZE, "ip addr add dev %s %s peer %s", tun_name, tun_local_addr, tun_peer_addr);
+	shell(cmdline);
+
+	relay(sd, tun_fd, &peer_addr);
+
+	close(sd);
+	close(tun_fd);
+
+	return 0;
+}
+
---
+++ protocol.h
@@ -0,0 +1,7 @@
+#ifndef PROTOCOL_H
+#define PROTOCOL_H
+
+#define	SOCKET_PORT	19900
+
+#endif
+
