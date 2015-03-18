3f96ae663e50de7af62094ecd724e2b4a5d5e0bd
牛海青 <haiqing1@staff.sina.com.cn>
2015-03-18 19:23:42 +0800

    Designing packet format.

--- protocol.h
+++ protocol.h
@@ -1,7 +1,30 @@
 #ifndef PROTOCOL_H
 #define PROTOCOL_H
 
-#define	SOCKET_PORT	19900
+enum {
+	CODE_DATA=0,
+	CODE_PING,
+	CODE_PONG,
+	CODE_FEC_SEG,
+};
+
+struct frame_st {
+	uint8_t code;
+	union {
+		struct {
+			uint64_t timestamp_ms;
+		} echo_body;
+		struct {
+			uint8_t data[0];
+		} data_body;
+		struct {
+			uint64_t fecg_id;
+			uint8_t fecg_seq;
+			uint8_t fecg_size;
+			uint8_t fecg_seg[0];
+		} fec_segment;
+	}
+};
 
 #endif
 
