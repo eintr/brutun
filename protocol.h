42b0cdd9b3f8ca42319b23a28dfbf5272d9a416c
牛海青 <haiqing1@staff.sina.com.cn>
2015-03-20 11:31:00 +0800

    Working on #3

--- protocol.h
+++ protocol.h
@@ -5,6 +5,7 @@
 
 enum {
 	CODE_DATA=0,
+	CODE_HELLO,
 	CODE_PING,
 	CODE_PONG,
 	CODE_FEC_SEG,
@@ -13,6 +14,12 @@ enum {
 struct frame_st {
 	uint8_t code;
 	union {
+		struct {
+			uint8_t recver_tun_addr[16];
+			uint8_t sender_tun_addr[16];
+			uint32_t salt;
+			uint32_t crc32;
+		} hello_body;
 		struct {
 			uint64_t timestamp_ms;
 		} echo_body;
